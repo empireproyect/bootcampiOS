86a9b558422acc3a74fc34b93cdf7b43e009314a
Ricardo Hernández Cambrón <riheca34@gmail.com>
2021-10-01 23:13:01 +0200

    subo .gitignore, modificaciones Model y Main.storyboard, despues hago rama developrhc

---
+++ sportpop/sportpop/Model/SportpopModel.swift
@@ -0,0 +1,19 @@
+//
+//  SportpopModel.swift
+//  sportpop
+//
+//  Created by Ricardo Hernandez on 30/9/21.
+//
+
+import Foundation
+
+struct User {
+    var username: String
+    var password: String
+    var email: String
+}
+
+struct Product {
+    var uid: String
+    var 
+}
