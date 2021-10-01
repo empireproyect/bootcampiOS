13424087cdea858b8d23f122546b7d223ccca213
Ricardo Hernández Cambrón <riheca34@gmail.com>
2021-10-01 23:36:24 +0200

    Model definition creada, structs

--- sportpop/sportpop/Model/SportpopModel.swift
+++ sportpop/sportpop/Model/SportpopModel.swift
@@ -8,12 +8,26 @@
 import Foundation
 
 struct User {
-    var username: String
+    var username: String // es posible que no haga falta, el email es el login
     var password: String
     var email: String
+    var displayname: String
+    var first_name: String
+    var last_name: String
+    var photo_url: String
+    var social_facebook_id: String
+    var social_apple_id: String
 }
 
 struct Product {
     var uid: String
-    var 
+    var pid: String
+    var price: Double
+    var category_description: String
+    var category_name: String
+    var description: String
+    var imagen_detail_url: String
+    var photo_url: String
+    var title: String
+    
 }
