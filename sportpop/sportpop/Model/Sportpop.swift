4e73d39dad04c8708d75d7bfc2c1d21a4d8f14d0
Ricardo Hernández Cambrón <riheca34@gmail.com>
2021-10-10 09:57:47 +0200

    subo modelo que se pierde abajo y no se porque.. Sportpop

---
+++ sportpop/sportpop/Model/Sportpop.swift
@@ -0,0 +1,35 @@
+//
+//  SportpopModel.swift
+//  sportpop
+//
+//  Created by Ricardo Hernandez on 30/9/21.
+//
+
+import Foundation
+
+struct User {
+    var username: String // es posible que no haga falta, el email es el login
+    var password: String
+    var email: String
+    var displayname: String
+    var first_name: String
+    var last_name: String
+    var photo_url: String
+    var social_facebook_id: String
+    var social_apple_id: String
+}
+
+struct Product {
+    var uid: String
+    var pid: String
+    var price: Double
+    var category_description: String
+    var category_name: String
+    var description: String
+    var imagen_detail_url: String
+    var photo_url: String
+    var title: String
+    
+}
+
+
