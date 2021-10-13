bdf8680e654ffa64e169de6ef548ce971b8d5868
Ricardo Hernández Cambrón <riheca34@gmail.com>
2021-10-13 19:57:37 +0200

    nueva pantalla sin empezar, sin firebase

---
+++ sportpop/sportpop/Controller/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  sportpop
+//
+//  Created by Ricardo Hernandez on 13/10/21.
+//
+
+import UIKit
+
+
+class SecondViewController: UIViewController {
+
+    
+    //let loginViewModel =
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+       
+    }
+
+}
+
